94739380b8aca3a16042727ea3c288a5f685ac5a
비디 심정범 <jbshim@bluedigm.com>
2019-12-11 11:26:31 +0900

    add script to reset certificates

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -5,6 +5,10 @@ export CAAS_DEPLOYMENT_NAME='paasta-container-service'
 export CAAS_BOSH2_NAME='micro-bosh'
 export CAAS_BOSH2_UUID=`bosh int <(bosh -e ${CAAS_BOSH2_NAME} environment --json) --path=/Tables/0/Rows/0/uuid`
 
+# RESET CREDENTIAL
+credhub delete -n /${CAAS_BOSH2_NAME}/${CAAS_DEPLOYMENT_NAME}/tls-kubernetes
+credhub delete -n /${CAAS_BOSH2_NAME}/${CAAS_DEPLOYMENT_NAME}/tls-kubelet
+
 # DEPLOY
 bosh -e ${CAAS_BOSH2_NAME} -n -d ${CAAS_DEPLOYMENT_NAME} deploy --no-redact manifests/paasta-container-service-deployment-aws.yml \
     -l manifests/paasta-container-service-vars-aws.yml \
