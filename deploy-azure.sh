30bcb0a987176cd701cad3523bc804b40cba8ae5
비디 심정범 <jbshim@bluedigm.com>
2019-12-11 20:30:10 +0900

    update deployment script

--- deploy-azure.sh
+++ deploy-azure.sh
@@ -5,10 +5,6 @@ export CAAS_DEPLOYMENT_NAME='paasta-container-service'
 export CAAS_BOSH2_NAME='micro-bosh'
 export CAAS_BOSH2_UUID=`bosh int <(bosh -e ${CAAS_BOSH2_NAME} environment --json) --path=/Tables/0/Rows/0/uuid`
 
-# RESET CERTIFICATES
-credhub delete -n /${CAAS_BOSH2_NAME}/${CAAS_DEPLOYMENT_NAME}/tls-kubernetes
-credhub delete -n /${CAAS_BOSH2_NAME}/${CAAS_DEPLOYMENT_NAME}/tls-kubelet
-
 # DEPLOY
 bosh -e ${CAAS_BOSH2_NAME} -n -d ${CAAS_DEPLOYMENT_NAME} deploy --no-redact manifests/paasta-container-service-deployment-azure.yml \
     -l manifests/paasta-container-service-vars-azure.yml \
